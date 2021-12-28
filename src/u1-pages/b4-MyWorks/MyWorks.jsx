057e29801a247a20843c1d13bfc99782af436a38
Артем <kysapka@gmail.com>
2021-12-28 22:41:12 +0300

    Edit some projects

--- src/u1-pages/b4-MyWorks/MyWorks.jsx
+++ src/u1-pages/b4-MyWorks/MyWorks.jsx
@@ -19,19 +19,27 @@ export const MyWorks = () => {
                 />
                 <div className={s.works}>
                     <Work style={oneWork}
-                          link={'https://kysapka.github.io/myfirstreact'}
-                          repLink={'https://github.com/Kysapka/myfirstreact'}
+                          link={'https://kysapka.github.io/social_network_ts'}
+                          repLink={'https://github.com/Kysapka/social_network_ts'}
                           projectName={'Social Network'}
                           description={
                               'It is the biggest project which I update every month and learn new technologies. It helps me to be on trend.'
                           }
                     />
                     <Work style={oneWork}
-                          link={'https://github.com/Kysapka/Todolist'}
+                          link={'https://kysapka.github.io/Todolist/'}
                           repLink={'https://github.com/Kysapka/Todolist'}
                           projectName={'Todo List'}
                           description={
                               'Redux-toolkit contains the best practice from all React developers. This project shows redux-toolkit and its strict limits.'
+                          }
+                    />
+                    <Work style={oneWork}
+                          link={'https://kysapka.github.io/cards-nya-front-2-0/'}
+                          repLink={'https://github.com/Kysapka/cards-nya-front-2-0'}
+                          projectName={'Flash-cards'}
+                          description={
+                              'This project helps to study with flash cards. Joint project, a team of 3 people. Technologies that used here : TypeScript,React, Redux, Redux-thunk, Hooks, Axios.'
                           }
                     />
                     <Work style={oneWork}
@@ -48,14 +56,6 @@ export const MyWorks = () => {
                           projectName={'Counter app'}
                           description={
                               'A training project using the local storage for a better understanding of the work of hooks and asynchronous logic'
-                          }
-                    />
-                    <Work style={oneWork}
-                          link={'https://samurai-hakaton.web.app/'}
-                          repLink={'https://github.com/Kysapka/hackaton'}
-                          projectName={'Hackaton project'}
-                          description={
-                              'Hackathon digital breakthrough. A social project for children from orphanages.'
                           }
                     />
                     <Work style={oneWork}
